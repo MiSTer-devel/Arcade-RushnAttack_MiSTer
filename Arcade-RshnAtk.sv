71eeb15482cb0e22590deea2d45b538b5c740162
MrX-8B <misterx6809@gmail.com>
2019-12-29 15:58:03 +0900

    Update sys

--- Arcade-RshnAtk.sv
+++ Arcade-RshnAtk.sv
@@ -14,7 +14,7 @@ module emu
 	input         RESET,
 
 	//Must be passed to hps_io module
-	inout  [44:0] HPS_BUS,
+	inout  [45:0] HPS_BUS,
 
 	//Base video clock. Usually equals to CLK_SYS.
 	output        VGA_CLK,
@@ -29,6 +29,7 @@ module emu
 	output        VGA_HS,
 	output        VGA_VS,
 	output        VGA_DE,    // = ~(VBlank | HBlank)
+	output		  VGA_F1,
 
 	//Base video clock. Usually equals to CLK_SYS.
 	output        HDMI_CLK,
@@ -59,9 +60,19 @@ module emu
 
 	output [15:0] AUDIO_L,
 	output [15:0] AUDIO_R,
-	output        AUDIO_S    // 1 - signed audio samples, 0 - unsigned
-);
+	output        AUDIO_S,   // 1 - signed audio samples, 0 - unsigned
 
+	// Open-drain User port.
+	// 0 - D+/RX
+	// 1 - D-/TX
+	// 2..6 - USR2..USR6
+	// Set USER_OUT to 1 to read from USER_IN.
+	input   [6:0] USER_IN,
+	output  [6:0] USER_OUT
+);	
+
+assign VGA_F1    = 0;
+assign USER_OUT  = '1;
 assign LED_USER  = ioctl_download;
 assign LED_DISK  = 0;
 assign LED_POWER = 0;
@@ -74,7 +85,7 @@ localparam CONF_STR = {
 	"A.RshnAtk;;",
 	"F,rom;", // allow loading of alternate ROMs
    "-;",
-	"O1,Aspect Ratio,Original,Wide;",
+	"H0O1,Aspect Ratio,Original,Wide;",
 	"O35,Scandoubler Fx,None,HQ2x,CRT 25%,CRT 50%,CRT 75%;",
 	"-;",
 	"O89,Lives,2,3,5,7;",
@@ -100,16 +111,15 @@ wire	[7:0]	DSW2 = 8'hFF;
 
 ////////////////////   CLOCKS   ///////////////////
 
-wire clk_hdmi;
 wire clk_48M;
-wire clk_sys = clk_hdmi;
+wire clk_hdmi = clk_48M;
+wire clk_sys  = clk_48M;
 
 pll pll
 (
 	.rst(0),
 	.refclk(CLK_50M),
-	.outclk_0(clk_48M),
-	.outclk_1(clk_hdmi)
+	.outclk_0(clk_48M)
 );
 
 ///////////////////////////////////////////////////
@@ -117,6 +127,7 @@ pll pll
 wire [31:0] status;
 wire  [1:0] buttons;
 wire        forced_scandoubler;
+wire			direct_video;
 
 wire        ioctl_download;
 wire        ioctl_wr;
@@ -126,6 +137,8 @@ wire  [7:0] ioctl_dout;
 wire [10:0] ps2_key;
 wire [15:0] joystk1, joystk2;
 
+wire [21:0]	gamma_bus;
+
 hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
 (
 	.clk_sys(clk_sys),
@@ -134,9 +147,14 @@ hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
 	.conf_str(CONF_STR),
 
 	.buttons(buttons),
-	.status(status),
-	.forced_scandoubler(forced_scandoubler),
 
+	.status(status),
+	.status_menumask({15'h0,direct_video}),
+	
+	.forced_scandoubler(forced_scandoubler),
+	.gamma_bus(gamma_bus),
+	.direct_video(direct_video),
+	
 	.ioctl_download(ioctl_download),
 	.ioctl_wr(ioctl_wr),
 	.ioctl_addr(ioctl_addr),
