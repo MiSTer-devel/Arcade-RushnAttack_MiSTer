80323a25539c802d1a0b9305449673a211b6da29
MikeS11 <mikesimone3@gmail.com>
2023-03-15 21:25:25 -0600

    Update Sys, Fix Aspect Ratio

--- Arcade-RshnAtk.sv
+++ Arcade-RshnAtk.sv
@@ -15,7 +15,7 @@ module emu
 	input         RESET,
 
 	//Must be passed to hps_io module
-	inout  [45:0] HPS_BUS,
+	inout  [48:0] HPS_BUS,
 
 	//Base video clock. Usually equals to CLK_SYS.
 	output        CLK_VIDEO,
@@ -38,13 +38,14 @@ module emu
 	output        VGA_F1,
 	output [1:0]  VGA_SL,
 	output        VGA_SCALER, // Force VGA scaler
+	output        VGA_DISABLE, // analog out is off
 
 	input  [11:0] HDMI_WIDTH,
 	input  [11:0] HDMI_HEIGHT,
 	output        HDMI_FREEZE,
 
 `ifdef MISTER_FB
-	// Use framebuffer in DDRAM (USE_FB=1 in qsf)
+	// Use framebuffer in DDRAM
 	// FB_FORMAT:
 	//    [2:0] : 011=8bpp(palette) 100=16bpp 101=24bpp 110=32bpp
 	//    [3]   : 0=16bits 565 1=16bits 1555
@@ -176,8 +177,8 @@ assign HDMI_FREEZE = 0;
 
 wire [1:0] ar = status[7:6];
 
-assign VIDEO_ARX =  (!ar) ? ( 12'd10) : (ar - 1'd1);
-assign VIDEO_ARY =  (!ar) ? ( 12'd7) : 12'd0;
+assign VIDEO_ARX =  (!ar) ? ( 12'd15) : (ar - 1'd1);
+assign VIDEO_ARY =  (!ar) ? ( 12'd14) : 12'd0;
 
 
 `include "build_id.v"
